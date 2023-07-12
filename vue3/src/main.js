9c6b480d95c2007575a2e0800c6e95ac5a1ea8aa
zequanwu <zequanwu@xiaoe-tech.com>
2023-07-12 17:40:19 +0800

    feat: vue3子应用

---
+++ vue3/src/main.js
@@ -0,0 +1,32 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+let instance = null;
+const render = (props = {}) => {
+    const { container } = props;
+    instance = createApp(App);
+    instance.mount(container || '#app')
+}
+
+console.log(window.__MINI_QIANKUN_LOADED__, 'window.__MINI_QIANKUN_LOADED__')
+
+if (!window.__MINI_QIANKUN_LOADED__) {
+    render();
+}
+
+const mount = (props) => {
+    console.log(props);
+    render(props);
+}
+
+const unmount = () => {
+    console.log(instance)
+    instance.unmount();
+    instance = null;
+}
+
+// TODO: 目前umd导出mount、unmount无法挂载在window上
+
+window.__MOUNT__ = mount;
+window.__UNMOUNT__ = unmount;
